b9fc67d32eb313ab808d285d07447431400e0943
Przemysław Kłos <pklos1992@gmail.com>
2024-01-18 18:46:24 +0100

    Show all posts in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,16 +1,31 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Posts') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ "Hey Admin" }}
-                </div>
+                @foreach ($posts as $post)
+                    <div class="p-6 text-gray-900">
+                        <div class="inline-flex">
+                            <form method="post" action="" class="mr-1">
+                                <input type="hidden" name="post_id" value="" />
+                                <x-primary-button>Edit</x-primary-button>
+                            </form>
+                            <form method="post" action="">
+                                <input type="hidden" name="post_id" value="" />
+                                <x-primary-button>Delete</x-primary-button>
+                            </form>
+                        </div>
+                        <h1 class="font-bold text-xl"> {{ $post->title }} </h1>
+                        <div class="mt-3">
+                            <p> {{ $post->content }} </p>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,9 +22,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/admin/dashboard', function() {
-    return view('admin.dashboard');
-})->middleware(['auth', 'role:admin'])->name('admin.dashboard');
+Route::get('/admin/dashboard', [PostsController::class, 'index'])->middleware(['auth', 'role:admin'])->name('admin.dashboard'); // TODO: make group
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
