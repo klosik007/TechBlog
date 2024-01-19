f94a92236b1afa951de2b4e08f30c3eb56f69834
Przemysław Kłos <pklos1992@gmail.com>
2024-01-19 12:32:10 +0100

    Dashboard post in separate rectangle
    Buttons colors fix

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,26 +7,30 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                @foreach ($posts as $post)
-                    <div class="p-6 text-gray-900">
-                        <div class="inline-flex">
-                            <form method="post" action="" class="mr-1">
-                                <input type="hidden" name="post_id" value="" />
-                                <x-primary-button>Edit</x-primary-button>
-                            </form>
-                            <form method="post" action="">
-                                <input type="hidden" name="post_id" value="" />
-                                <x-primary-button>Delete</x-primary-button>
-                            </form>
-                        </div>
-                        <h1 class="font-bold text-xl"> {{ $post->title }} </h1>
-                        <div class="mt-3">
-                            <p> {{ $post->content }} </p>
-                        </div>
+            <form method="post" action="" class="mb-5">
+                <input type="hidden" name="post_id" value="" />
+                <x-secondary-button>Add Post</x-primary-button>
+            </form>
+            @foreach ($posts as $post)
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-3">
+                <div class="p-6 text-gray-900">
+                    <div class="inline-flex">
+                        <form method="post" action="" class="mr-1">
+                            <input type="hidden" name="post_id" value="" />
+                            <x-primary-button>Edit</x-primary-button>
+                        </form>
+                        <form method="post" action="">
+                            <input type="hidden" name="post_id" value="" />
+                            <x-danger-button>Delete</x-primary-button>
+                        </form>
                     </div>
-                @endforeach
+                    <h1 class="font-bold text-xl"> {{ $post->title }} </h1>
+                    <div class="mt-3">
+                        <p> {{ $post->content }} </p>
+                    </div>
+                </div>
             </div>
+            @endforeach
         </div>
     </div>
 </x-app-layout>
