94d509c6ba6553e92a2dc4eb6214b7fa6adbd364
Przemysław Kłos <pklos1992@gmail.com>
2024-04-17 10:03:54 +0200

    More rows in post input/edit (using textarea markup)

--- resources/views/post/add.blade.php
+++ resources/views/post/add.blade.php
@@ -9,10 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-3">
                 <div class="p-6 text-gray-900">
-                    <form action="{{ route('post.store') }}" method="post">
+                    <form id="add_post_form" action="{{ route('post.store') }}" method="post">
                         @csrf
                         <x-text-input field="title" name="title" type="text" placeholder="Title" class="w-full" autocomplete="off" :value="@old('title')"></x-text-input>
-                        <x-textarea field="text" name="text" rows="10" placeholder="Start typing here..." class="w-full" :value="@old('text')"></x-textarea>
+                        <x-textarea form="add_post_form" field="text" name="text" rows="10" placeholder="Start typing here..." class="w-full" :text="@old('text')"></x-textarea>
                         <x-primary-button>Add</x-primary-button>
                     </form>
                 </div>
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -9,11 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-3">
                 <div class="p-6 text-gray-900">
-                    <form action="{{ route('post.update', $post) }}" method="post">
+                    <form id="edit_post_form" action="{{ route('post.update', $post) }}" method="post">
                         @method('put')
                         @csrf
                         <x-text-input field="title" name="title" type="text" placeholder="Title" class="w-full" autocomplete="off" :value="@old('title', $post->title)"></x-text-input>
-                        <x-textarea field="text" name="text" rows="10" placeholder="Start typing here..." class="w-full" :value="@old('text', $post->content)"></x-textarea>
+                        <x-textarea form="edit_post_form" field="text" name="text" rows="10" placeholder="Start typing here..." class="w-full" :text="@old('text', $post->content)"></x-textarea>
                         <x-primary-button>Save</x-primary-button>
                     </form>
                 </div>
